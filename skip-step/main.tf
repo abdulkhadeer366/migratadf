c7b0b15d317e96819e5750697c79639ad6a554ad
abdulkhadeer366 <77782993+abdulkhadeer366@users.noreply.github.com>
2021-04-08 18:12:08 +0530

    Added resource adf

--- skip-step/main.tf
+++ skip-step/main.tf
@@ -13,3 +13,9 @@ resource "azurerm_resource_group" "rg" {
   name     = "resourcegroup-test-tbd-1"
   location = "westeurope"
 }
+
+resource "azurerm_data_factory" "adf" {
+  name                = var.adfname
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+}
